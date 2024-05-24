2c614aea65ea2f6f85992ce18f74432b3b251ab5
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-24 16:20:20 -0300

    chore: update express configuration and view engine setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 require('dotenv').config();
+const path = require('path');
+
+const app = express();
 const port = process.env.PORT || 3001;
 
+app.set('view engine', 'ejs');
 
-
+app.set('views',path.join(__dirname,'views'));  
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
